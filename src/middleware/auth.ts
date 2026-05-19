543ca91bea8c09ba055ca41480b7d1b880a5eddc
Md Saiful Haque <saifulhaque.pciu@gmail.com>
2026-05-19 11:30:15 +0600

    auth middleware validation

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,5 +1,7 @@
 import type { NextFunction, Request, Response } from "express";
-
+import jwt, { type JwtPayload } from "jsonwebtoken"
+import config from "../config";
+import { pool } from "../db";
 
 const auth = () => {
     return async (req: Request, res: Response, next: NextFunction) => {
@@ -14,6 +16,31 @@ const auth = () => {
                 message: "Unauthorize access!!"
             })
         }
+
+        const decoded = jwt.verify(token as string, config.secret as string) as JwtPayload;
+        // console.log(decoded);
+
+        const userData = await pool.query(`
+            SELECT * FROM users WHERE email=$1
+            `, [decoded.email])
+
+        const user = userData.rows[0];
+
+        // console.log(user);
+        if (userData.rows.length === 0) {
+            res.status(404).json({
+                success: false,
+                message: "User Not Found!"
+            })
+        }
+
+        if (!user.is_active) {
+            res.status(403).json({
+                success: false,
+                message: "Forbidden!"
+            })
+        }
+
         next()
     }
 }
